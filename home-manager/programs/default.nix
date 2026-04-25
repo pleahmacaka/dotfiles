d1c7757c83e6b147306f24ccb25b883000b0ec20
PleahMaCaka <pleahmacaka@gmail.com>
2026-04-26 08:19:26 +0900

    feat(home-manager): add theme config

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -7,5 +7,6 @@
     ./hyprland.nix
     ./alacritty.nix
     ./xdg.nix
+    ./theme.nix
   ];
 }
---
+++ home-manager/programs/theme.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    gtk4.theme = config.gtk.theme;
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
+    };
+  };
+}
