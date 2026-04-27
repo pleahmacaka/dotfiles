276793ab1383e22e1a4ac1353a9c112bc4660c2f
PleahMaCaka <pleahmacaka@gmail.com>
2026-04-27 23:57:03 +0900

    chore(nix): enable determinate, lazy-trees, caching, btop cuda

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     wuw.url = "github:PleahMaCaka/wuw";
     wuw.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -13,6 +14,7 @@
       nixpkgs,
       nixos-wsl,
       home-manager,
+      determinate,
       wuw,
       ...
     }:
@@ -25,6 +27,7 @@
           specialArgs = extraArgs;
           modules = [
             ./modules/default.nix
+            determinate.nixosModules.default
             home-manager.nixosModules.home-manager
             {
               networking.hostName = hostname;
--- modules/common/default.nix
+++ modules/common/default.nix
@@ -7,12 +7,47 @@
     ./users.nix
   ];
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" "lazy-trees" "parallel-eval" ];
+    eval-cache = true;
+
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+
+    builders-use-substitutes = true;
+    keep-outputs = true;
+    keep-derivations = true;
+
+    auto-optimise-store = true;
+
+    max-jobs = "auto";
+    cores = 0;
+
+    http-connections = 50;
+    max-substitution-jobs = 32;
+    download-buffer-size = 268435456;
+    connect-timeout = 5;
+
+    trusted-users = [ "root" "@wheel" ];
+  };
 
   nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.overlays = [
+    (self: super: {
+      btop = super.btop.override { cudaSupport = true; };
+    })
+  ];
 
   time.timeZone = "Asia/Seoul";
 
@@ -36,6 +71,7 @@
     nixd
     nix-output-monitor
     just
+    fastfetch
   ];
 
   security.polkit.enable = true;
